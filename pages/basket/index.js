7ff852f3942dfb08d567393263777dc0ae2cc1d9
Ivan Sevilla <ivansevillaa@gmail.com>
2020-08-30 19:35:03 -0300

    added basket page

---
+++ pages/basket/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Basket = () => {
+  return (
+    <main>
+      <h1>🛠 Working 🛠</h1>
+
+      <style jsx>{`
+        h1 {
+          text-align: center;
+        }
+      `}</style>
+    </main>
+  );
+};
+
+export default Basket;
